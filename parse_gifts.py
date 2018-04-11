0972eeb797be03b850c4620e3b2557c0cdbb2300
Lucas Brandstaetter <lucas@brandstaetter.tech>
2018-04-11 23:04:16 +0200

    Add python linting

--- parse_gifts.py
+++ parse_gifts.py
@@ -19,11 +19,11 @@ def main():
                 regex = re.compile(
                     r'(.*?)/(.*?/){2}([A-Za-z ]*)(\s([0-9\-]*))*/.*')
                 for itemid, itemdata in items.items():
-                    m = regex.match(itemdata)
-                    if m.group(4):
-                        catid = int(m.group(5))
+                    match = regex.match(itemdata)
+                    if match.group(4):
+                        catid = int(match.group(5))
                         data[catid] = data.get(catid, {})
-                        data[catid]['name'] = m.group(3)
+                        data[catid]['name'] = match.group(3)
                         data[catid]['items'] = data[catid].get('items', [])
                         data[catid]['items'].append(int(itemid))
             except yaml.YAMLError as exc:
@@ -65,7 +65,7 @@ def main():
 
                 regex = re.compile(r'.+?/(.*?)/')
                 for person, taste in rawdata.items():
-                    logger.info("Calculating likes for {}".format(person))
+                    logger.info("Calculating likes for %s", person)
                     tastes[person] = copy.deepcopy(tastes['universal'])
                     taste = regex.findall(taste)
                     taste = [v.split() for v in taste]
@@ -84,18 +84,19 @@ def main():
                 logger.fatal(exc)
 
         logger.info("Begin data association")
-        del(tastes['universal'])
+        del tastes['universal']
         itemtastes = {}
         for person, taste in tastes.items():
             categories = set(data.keys()).intersection(taste['cats'].keys())
             items = {
                 **{itemid: taste['cats'][catid]
                    for catid in categories for itemid in data[catid]['items']},
-                       **taste['items']}
+                **taste['items']}
             itemtastes[person] = items
             print("{}: {}".format(person, len(items)))
         for person in itemtastes:
-            itemtastes[person] = {k: v for k, v in itemtastes[person].items() if v < 2 or v == 4}
+            itemtastes[person] = {
+                k: v for k, v in itemtastes[person].items() if v < 2 or v == 4}
         with open('NPCGiftTastes.json', 'w') as outfile:
             json.dump(itemtastes, outfile, indent=2, sort_keys=True)
     except FileNotFoundError as exc:
