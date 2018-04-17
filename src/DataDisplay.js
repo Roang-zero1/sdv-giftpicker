e4b322cc72a7a9726503d9ea3b1a65e775b8e304
Lucas <git@roang.info>
2018-04-17 21:00:15 +0200

    Add category looping
    Loop through categories and introduce error checking.

--- src/DataDisplay.js
+++ src/DataDisplay.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import classNames from 'classnames';
 import './DataDisplay.css';
-import tastes from './GiftTastes.js'
+import tastes from './GiftTastes.js';
+
+const categories_map = {
+  0: 'Love',
+  1: 'Like',
+  4: 'neutral'
+};
 
 class DataDisplay extends Component {
   render(props) {
     var characters = [];
-    for (var char in this.props.charactersData) {
+    for (var char in tastes) {
       if (this.props.charactersData[char] < 2) {
-        {
-          /* TODO: loop through categories automatically*/
+        var categories_output = [];
+        for (var cat in categories_map) {
+          categories_output.push(this.renderGiftCategories(char, cat));
         }
-        var love = this.renderGiftCategories(char, 0);
-        var like = this.renderGiftCategories(char, 1);
-        var neutral = this.renderGiftCategories(char, 2);
 
         characters.push(
           <div key={char}>
@@ -30,18 +34,7 @@ class DataDisplay extends Component {
               Gifts given {this.props.charactersData[char] > 0 ? 'X' : 'O'}
               {this.props.charactersData[char] > 1 ? 'X' : 'O'}
             </p>
-            <Container>
-              <h4>Love</h4>
-              <Row>{love}</Row>
-            </Container>
-            <Container>
-              <h4>Like</h4>
-              <Row>{like}</Row>
-            </Container>
-            <Container>
-              <h4>Neutral</h4>
-              <Row>{neutral}</Row>
-            </Container>
+            {categories_output}
           </div>
         );
       }
@@ -55,35 +48,48 @@ class DataDisplay extends Component {
   }
 
   renderGiftCategories(char, category) {
-    var characterTastes = tastes[char][category];
-    var gifts = [];
-    for (var gift in characterTastes) {
-      const itemID = characterTastes[gift];
-      gifts.push(
-        <Col
-          xs="12"
-          sm="6"
-          lg="4"
-          xl="3"
-          className={classNames({
-            item: true,
-            missing: !this.props.giftsData[itemID].count
-          })}
-          key={itemID}
-        >
-          <Row>
-          <Col xs="1"><img
-            className="icon"
-            src={require('./images/items/' + this.props.giftsData[itemID].name + '.png')}
-            alt=""
-          /></Col>
-          <Col xs="auto">{this.props.giftsData[itemID].displayName}</Col>
-          <Col xs="3" align-self="end" className="count">{this.props.giftsData[itemID].count}</Col>
-          </Row>
-        </Col>
+    if (char in tastes && category in tastes[char]) {
+      var characterTastes = tastes[char][category];
+      var gifts = [];
+      for (var gift in characterTastes) {
+        const itemID = characterTastes[gift];
+        gifts.push(
+          <Col
+            xs="12"
+            sm="6"
+            lg="4"
+            xl="3"
+            className={classNames({
+              item: true,
+              missing: !this.props.giftsData[itemID].count
+            })}
+            key={itemID}
+          >
+            <Row>
+              <Col xs="1">
+                <img
+                  className="icon"
+                  src={require('./images/items/' +
+                    this.props.giftsData[itemID].name +
+                    '.png')}
+                  alt=""
+                />
+              </Col>
+              <Col xs="auto">{this.props.giftsData[itemID].displayName}</Col>
+              <Col xs="3" align-self="end" className="count">
+                {this.props.giftsData[itemID].count}
+              </Col>
+            </Row>
+          </Col>
+        );
+      }
+      return (
+        <Container>
+          <h4>{categories_map[category]}</h4>
+          <Row>{gifts}</Row>
+        </Container>
       );
     }
-    return gifts;
   }
 }
 
