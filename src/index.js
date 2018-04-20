656ae5db37d8601ce903e4f634c9c6034ec3cda4
Lucas <git@roang.info>
2018-04-20 17:23:06 +0200

    Add state persistance

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './store/configureStore';
+import registerServiceWorker from './registerServiceWorker';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,18 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import rootReducer from '../reducers/rootReducer';
 
-export default function configureStore() {
-  return createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-}
+const persistConfig = {
+  key: 'sdv-gp',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+export const persistor = persistStore(store);
