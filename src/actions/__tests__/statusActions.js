3432d00f7adb28308fecd4b7d32fb89988326814
Lucas <git@roang.info>
2018-04-30 21:01:21 +0200

    Add statusActions tests

---
+++ src/actions/__tests__/statusActions.js
@@ -0,0 +1,52 @@
+import * as actions from '../statusActions';
+import * as types from '../actionTypes';
+
+describe('charactersActions', () => {
+  it('should create an action to set the intro status', () => {
+    const expectedAction = {
+      type: types.SET_INTRO_CHOSEN,
+      intro: true
+    };
+    expect(actions.setIntroChosen(true)).toEqual(expectedAction);
+  });
+
+  it('should create an action to set the default intro status', () => {
+    const expectedAction = {
+      type: types.SET_INTRO_CHOSEN,
+      intro: false
+    };
+    expect(actions.setIntroChosen()).toEqual(expectedAction);
+  });
+
+  it('should create an action to set the save game status', () => {
+    const expectedAction = {
+      type: types.SET_SAVE_GAME,
+      save: true
+    };
+    expect(actions.setSaveGame(true)).toEqual(expectedAction);
+  });
+
+  it('should create an action to set the default save game status', () => {
+    const expectedAction = {
+      type: types.SET_SAVE_GAME,
+      save: false
+    };
+    expect(actions.setSaveGame()).toEqual(expectedAction);
+  });
+
+  it('should create an action to set the loading status', () => {
+    const expectedAction = {
+      type: types.SET_LOADING,
+      loading: true
+    };
+    expect(actions.setLoading(true)).toEqual(expectedAction);
+  });
+
+  it('should create an action to set the default loading status', () => {
+    const expectedAction = {
+      type: types.SET_LOADING,
+      loading: false
+    };
+    expect(actions.setLoading()).toEqual(expectedAction);
+  });
+});
