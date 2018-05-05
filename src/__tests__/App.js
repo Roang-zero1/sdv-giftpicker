4354a2f64d64eba5ac4c2054f95a99ebfd3d267e
Lucas <git@roang.info>
2018-05-05 19:58:19 +0200

    Move App tests to correct folder

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
-import App from './App';
+import App from '../App';
 
-import { store } from './store/configureStore';
+import { store } from '../store/configureStore';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
