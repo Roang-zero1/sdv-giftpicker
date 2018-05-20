469fa7fa0df182e6b2c25c8a5c23db11706a2a35
Lucas <git@roang.info>
2018-05-21 01:36:01 +0200

    Add typescript basics

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,9 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const rewireTypescript = require('react-app-rewire-typescript');
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
+  config = rewireTypescript(config, env);
   config = rewireReactHotLoader(config, env);
   return config;
 };
