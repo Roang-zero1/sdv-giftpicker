fe40a0a2799ec6e5efe2e54f8c1b389a3462d637
Lucas <git@roang.info>
2018-05-21 17:41:17 +0200

    Refactor itemsReducer tests

--- src/reducers/__tests__/itemsReducer.ts
+++ src/reducers/__tests__/itemsReducer.ts
@@ -1,23 +1,22 @@
 import { modifyItemCount, updateItems } from '../../actions/itemsActions';
-import { IItems } from '../../common/types';
-import { initialState } from '../itemsReducer';
+import { initialState, IState } from '../itemsReducer';
 import reducer from '../itemsReducer';
 
-export interface IState {
-  readonly [itemID: number]: number;
-}
 describe('itemsReducer', () => {
   it('should update the item data with new value', () => {
-    const itemsData: IItems = { 200: 15, 208: 30 };
-    let state: IItems = { 150: 15 };
-    state = reducer(state, updateItems(itemsData));
-    expect(state).toEqual(itemsData);
+    const itemsData: IState = { 200: 15, 208: 30 };
+    const initialTestState: IState = { 150: 15 };
+    const newState: IState = reducer(initialTestState, updateItems(itemsData));
+    expect(newState).toEqual(itemsData);
   });
 
   it('should increment an item count', () => {
-    let state: IItems = { 150: 15 };
-    state = reducer(state, modifyItemCount({ itemID: 150, amount: 1 }));
-    expect(state).toEqual({ 150: 16 });
+    const initialTestState: IState = { 150: 15 };
+    const newState: IState = reducer(
+      initialTestState,
+      modifyItemCount({ itemID: 150, amount: 1 })
+    );
+    expect(newState).toEqual({ 150: 16 });
   });
 
   it('should return the initial state', () => {
