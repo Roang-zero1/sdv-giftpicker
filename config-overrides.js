876d11e791d33120450e974258195889f616c951
Lucas <git@roang.info>
2018-05-21 02:41:52 +0200

    Add typescript testing

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,7 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
-const rewireTypescript = require('react-app-rewire-typescript');
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
-  config = rewireTypescript(config, env);
   config = rewireReactHotLoader(config, env);
   return config;
 };
