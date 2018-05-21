da412a64281f09879dbd1c653593a3f74ae26a45
Lucas <git@roang.info>
2018-05-21 16:00:22 +0200

    Update index to typescript

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
+import { persistor, store } from './store/configureStore';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import AppRoutes from './AppRoutes';
+import Loader from './components/Loader';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={<Loader />} persistor={persistor}>
+      <AppRoutes />
+    </PersistGate>
+  </Provider>,
+
+  document.getElementById('root')
+);
+registerServiceWorker();
