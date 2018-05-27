95856f4cf819acda9937e5ecd3302432b708a263
Lucas <git@roang.info>
2018-05-27 22:36:46 +0200

    Update Icon to typescript

---
+++ src/components/Icon.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { Component } from 'react';
+import styled from 'styled-components';
+
+import images from '../data/GiftImages';
+
+export interface IProps {
+  gift: number;
+  grayscale?: boolean;
+}
+
+interface IImgProps {
+  grayscale?: boolean;
+}
+
+const Img = styled<IImgProps, 'img'>('img')`
+  left: -12px;
+  position: relative;
+  max-width: 24px;
+  filter: ${props => (props.grayscale ? 'grayscale(100%)' : undefined)};
+`;
+
+class Icon extends Component<IProps> {
+  public render() {
+    const { gift, grayscale } = this.props;
+    return <Img grayscale={grayscale} src={images[gift]} alt="" />;
+  }
+}
+
+export default Icon;
