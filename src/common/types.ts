6045a0957fb4041baa9bd0af296317523f2080d8
Lucas <git@roang.info>
2018-05-27 18:30:56 +0200

    Add state types

--- src/common/types.ts
+++ src/common/types.ts
@@ -4,6 +4,11 @@ import * as charactersActions from '../actions/charactersActions';
 import * as itemsAction from '../actions/itemsActions';
 import * as navigationActions from '../actions/navigationActions';
 import * as statusActions from '../actions/statusActions';
+import charactersReducer from '../reducers/charactersReducer';
+import itemsReducer from '../reducers/itemsReducer';
+import navigationReducer from '../reducers/navigationReducer';
+import rootReducer from '../reducers/rootReducer';
+import statusReducer from '../reducers/statusReducer';
 
 export interface IItems {
   [itemID: number]: number;
@@ -28,3 +33,9 @@ export type CharactersActions = ActionType<typeof charactersActions>;
 export type ItemsActions = ActionType<typeof itemsAction>;
 export type NavigationActions = ActionType<typeof navigationActions>;
 export type StatusActions = ActionType<typeof statusActions>;
+
+export type CharacterState = StateType<typeof charactersReducer>;
+export type ItemsState = StateType<typeof itemsReducer>;
+export type NavigationState = StateType<typeof navigationReducer>;
+export type StatusState = StateType<typeof statusReducer>;
+export type RootState = StateType<typeof rootReducer>;
